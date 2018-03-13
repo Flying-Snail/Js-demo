486ced4dacb93bdd5fdc8268cb5b4955744a801f
jiaSongLin <844030491@qq.com>
2018-03-13 13:58:11 +0800

    add Eslint

--- 软绑定/softBind.js
+++ 软绑定/softBind.js
@@ -1,30 +1,30 @@
+/* eslint-disable no-extend-native */
+
 if (!Function.prototype.softBind) {
   Function.prototype.softBind = function softBind(obj) {
     // obj每次调用时都为第一次调用的参数，这里相当于obj1
-    let fn = this;
-    let curried = [].slice.call( arguments, 1);
-    let bound = function () {
+    const fn = this;
+    const bound = function bound(...arg) {
       // 这里a的意义是当this是默认绑定时绑定自己定义的默认绑定，否则，就按照后来定义的绑定规则绑定
-      let a = (!this || this === (window || global)) ? obj : this;
+      const a = (!this || this === (window || global)) ? obj : this;
       // b是默认绑定之后传入的参数
-      let b = Array.prototype.slice(arguments);
-      return fn.apply(a,b);
+      const b = arg;
+      return fn.apply(a, b);
     };
     bound.prototype = Object.create(fn.prototype);
     return bound;
-  }
+  };
 }
 
 function foo() {
   console.log(this.name);
 }
 
-let obj1 = {name: "obj1"},
-    obj2 = {name: "obj2"};
-    obj3 = {name: "obj3"}
+const obj1 = { name: 'obj1' };
+const obj2 = { name: 'obj2' };
 
 // 这里进行了初值的设置
-let fooOBJ = foo.softBind(obj1);
+const fooOBJ = foo.softBind(obj1);
 // 在这可能需要了解一下柯里化【柯南柯里化，了解一下：）】
 // fooOBJ函数其实就是bound，这里this为window，所以默认绑定obj1
 fooOBJ();
@@ -34,4 +34,4 @@ obj2.foo = foo.softBind(obj1);
 obj2.foo();
 
 // this隐式丢失，软绑定obj1
-setTimeout(obj2.foo,10);
+setTimeout(obj2.foo, 10);
